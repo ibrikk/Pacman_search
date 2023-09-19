7a4cb0a870c4326c44764bd4f93f7d10ab52a0fc
Ibrahim Khalilov <helmut.rozenweig@gmail.com>
2023-09-19 15:15:59 -0400

    edit

--- search.py
+++ search.py
@@ -143,14 +143,14 @@ def aStarSearch(problem, heuristic=nullHeuristic):
     """Search the node that has the lowest combined cost and heuristic first."""
     "*** YOUR CODE HERE ***"
     astarPQ = util.PriorityQueue();
-    visitedSet = set();
+    visitedList = [];
     astarPQ.push((problem.getStartState(), []), 0);
     while not astarPQ.isEmpty():
         (node, path) = astarPQ.pop();
         if problem.isGoalState(node):
             return path;
-        if node not in visitedSet:
-            visitedSet.add(node);
+        if node not in visitedList:
+            visitedList.append(node);
             for child, direction, _ in problem.getSuccessors(node):
                 astarPQ.push((child, path + [direction]), problem.getCostOfActions(path + [direction]) + heuristic(child, problem));
     util.raiseNotDefined()
